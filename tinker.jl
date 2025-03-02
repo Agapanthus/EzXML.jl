f18c4df31acdbf400a9cdba87fb1a5b3f0391abf
Eric Skaliks <26843285+Agapanthus@users.noreply.github.com>
2025-03-02 12:53:01 +0100

    tinker

---
+++ tinker.jl
@@ -0,0 +1,28 @@
+using Pkg
+Pkg.activate(".")
+Pkg.add("TranscodingStreams")
+using TranscodingStreams
+using EzXML
+
+undirected = """
+<?xml version="1.0" encoding="UTF-8"?>
+<graphml>
+    <graph edgedefault="undirected">
+        <node id="n0"/>
+        <node id="n1"/>
+        <edge source="n0" target="n1"/>
+    </graph>
+</graphml>
+"""
+
+reader = EzXML.StreamReader(NoopStream(IOBuffer(undirected)))
+
+GC.gc()
+GC.gc()
+GC.gc()
+
+while (item = iterate(reader)) != nothing
+    @show reader.type, reader.name
+end
+
+close(reader)
